41cde6f2fab6d37fb1f972861b5b9242aca8009b
Melih <mlhtncc@gmail.com>
2025-08-23 16:55:52 +0300

    Implement basics of StreamScreen

--- src/screens/StreamScreen.tsx
+++ src/screens/StreamScreen.tsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 
 import { Colors } from '../constants';
 import { StreamScreenProps } from '../types';
+import Video from 'react-native-video';
 
+const { width } = Dimensions.get('window');
 
 export default function StreamScreen({ navigation, route }: StreamScreenProps) {
 
   const { channel } = route.params;
 
-  console.log(channel);
 
   useEffect(() => {
   }, []);
-
-  
+ 
 
   return (
     <View style={styles.container}>
+      <Video
+				source={{ uri: '' }}
+        style={{ width: '100%', aspectRatio: 16 / 9 }}
+        controls
+			/>
       
     </View>
   );
@@ -26,6 +31,13 @@ export default function StreamScreen({ navigation, route }: StreamScreenProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: Colors.background,
+		backgroundColor: '#000',
+		justifyContent: 'center',
+		alignItems: 'center',
   },
+  video: {
+		width: width,
+		height: (width * 9) / 16, // 16:9 ratio
+		backgroundColor: '#000',
+	},
 });
