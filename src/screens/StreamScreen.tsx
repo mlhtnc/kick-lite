7fefdc9c8160a392482e1f0cdfe16fe3bcbe158f
Melih <mlhtncc@gmail.com>
2025-08-24 17:15:05 +0300

    Update StreamScreen.tsx

--- src/screens/StreamScreen.tsx
+++ src/screens/StreamScreen.tsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, View, Dimensions } from 'react-native';
+import { StyleSheet, View, Dimensions, TouchableOpacity, Text } from 'react-native';
 import Video from 'react-native-video';
 
 import { StreamScreenProps } from '../types';
 import { getStreamURL } from '../services/backend_service';
 import Orientation from 'react-native-orientation-locker';
+import { Colors } from '../constants';
 
 
 const { width } = Dimensions.get('window');
@@ -14,7 +15,6 @@ export default function StreamScreen({ navigation, route }: StreamScreenProps) {
   const { channel } = route.params;
 
   const [ streamURL, setStreamURL ] = useState<string>("");
-
 
   useEffect(() => {
     fetchStreamURL();
@@ -38,6 +38,7 @@ export default function StreamScreen({ navigation, route }: StreamScreenProps) {
     Orientation.lockToPortrait();
   }
 
+
   return (
     <View style={styles.container}>
 
@@ -48,11 +49,10 @@ export default function StreamScreen({ navigation, route }: StreamScreenProps) {
           resizeMode='contain'
           onFullscreenPlayerWillPresent={onFullscreenPlayerWillPresent}
           onFullscreenPlayerWillDismiss={onFullscreenPlayerWillDismiss}
-          controls
         />
         :
         null
-      }
+      } 
       
     </View>
   );
@@ -61,13 +61,13 @@ export default function StreamScreen({ navigation, route }: StreamScreenProps) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-		backgroundColor: '#000',
+		backgroundColor: Colors.background,
 		justifyContent: 'flex-start',
 		alignItems: 'center',
   },
   video: {
 		width: width,
-		height: (width * 9) / 16, // 16:9 ratio
+		height: (width * 9) / 16,
 		backgroundColor: '#000',
 	},
 });
