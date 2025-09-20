a92ac2fab2efec64b08ea9303941390f22649fbd
Melih <mlhtncc@gmail.com>
2025-09-20 17:55:12 +0300

    Remove bottom tab bar from LoginScreen

--- src/navigations/MainTabs.tsx
+++ src/navigations/MainTabs.tsx
@@ -4,10 +4,10 @@ import Ionicons, { IoniconsIconName } from "@react-native-vector-icons/ionicons"
 
 import { RootTabParamList, Screens } from "../types";
 import { Colors } from "../constants";
-import MainStack from "./MainStack";
 import BrowseScreen from "../screens/BrowseScreen";
 import SleepTimerScreen from "../screens/SleepTimerScreen";
 import SearchScreen from "../screens/SearchScreen";
+import HomeScreen from "../screens/HomeScreen";
 
 
 const Tab = createBottomTabNavigator<RootTabParamList>();
@@ -24,7 +24,7 @@ export default function MainTabs() {
         tabBarIcon: ({ color, size }) => {
           let iconName: IoniconsIconName = 'home-outline';
 
-          if (route.name === Screens.MainStack) {
+          if (route.name === Screens.Home) {
             iconName = 'home-outline';
           } else if (route.name === Screens.Browse) {
             iconName = 'albums-outline';
@@ -38,7 +38,7 @@ export default function MainTabs() {
         }
       })}
     >
-      <Tab.Screen name={Screens.MainStack} component={MainStack} />
+      <Tab.Screen name={Screens.Home} component={HomeScreen} />
       <Tab.Screen name={Screens.Browse} component={BrowseScreen} />
       <Tab.Screen name={Screens.SleepTimer} component={SleepTimerScreen} />
       <Tab.Screen name={Screens.Search} component={SearchScreen} />
