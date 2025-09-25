6d62e70ffc8736291fa5478df0e4f5aad02dc521
Melih <mlhtncc@gmail.com>
2025-09-25 14:38:18 +0300

    Prevent unnecessary re-renders

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,94 +1,18 @@
-import { useEffect, useState } from 'react';
+import { useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
 
 import { Colors } from '../constants';
-import { Channel } from '../types';
-import { getChannels, getUser } from '../services/kick_service';
 import ChannelList from '../components/ChannelList';
-import { showErrorChannelsLoading, showErrorUserLoading } from '../alerts/alerts';
-import { loadChannels } from '../utils/save_utils';
 import { useChannelListStore } from '../stores/channelListStore';
 
 
 export default function HomeScreen() {
 
-  const [ loading, setLoading ] = useState<boolean>(false);
-  const { channels, setChannels } = useChannelListStore();
-
+  const { channels, channelsLoading, refreshChannels } = useChannelListStore();
 
   useEffect(() => {
-    loadSavedChannels();
+    refreshChannels();
   }, []);
-
-  useEffect(() => {
-    if(channels.length === 0) {
-      return;
-    }
-
-    fetchChannels();
-  }, [channels.length]);
-
-
-  const loadSavedChannels = async () => {
-    const savedChannels = await loadChannels();
-    if(savedChannels.length === 0) {
-      return;
-    }
-
-    setChannels(savedChannels);
-  }
-
-
-  const fetchChannels = async () => {
-    setLoading(true);
-    getChannels(channels.map((ch: Channel) => ch.slug))
-    .then(async (channels) => {
-      let updatedChannels = await fetchChannelUsernames(channels);
-      sortChannels(updatedChannels);
-      setChannels(updatedChannels);
-    }).catch(() => {
-      showErrorChannelsLoading();
-    }).finally(() => {
-      setLoading(false);
-    });
-  }
-
-  const fetchChannelUsernames = async (channels: Channel[]) => {
-    let error: boolean = false;
-
-    const updatedChannels = await Promise.all(
-      channels.map(async (ch) => {
-        try {
-          const user = await getUser(ch.id);
-          return { ...ch, name: user.name };
-        } catch (err) {
-          error = true;
-          return ch;
-        }
-      })
-    );
-
-    if(error) {
-      showErrorUserLoading();
-    }
-
-    return updatedChannels;
-  }
-
-  const sortChannels = (channels: Channel[]) => {
-    return channels.sort((a, b) => {
-      if (a.isLive !== b.isLive) {
-        return a.isLive ? -1 : 1;
-      }
-
-      return b.viewerCount - a.viewerCount;
-    });
-  }
-
-  const onRefresh = () => {
-    fetchChannels();
-  }
-
 
   return (
     <View style={styles.container}>
@@ -96,8 +20,8 @@ export default function HomeScreen() {
       <View style={styles.listContainer}>
         <ChannelList
           channels={channels || []}
-          loading={loading}
-          onRefresh={onRefresh}
+          loading={channelsLoading}
+          onRefresh={refreshChannels}
         />
       </View>
     
--- src/stores/channelListStore.ts
+++ src/stores/channelListStore.ts
@@ -1,7 +1,9 @@
-import { create } from 'zustand';
+import { create, StateCreator } from 'zustand';
 
 import { Channel } from '../types';
-import { saveChannels } from '../utils/save_utils';
+import { loadChannels, saveChannels } from '../utils/save_utils';
+import { getChannels, getUser } from '../services/kick_service';
+import { showErrorChannelsLoading, showErrorUserLoading } from '../alerts/alerts';
 
 
 interface ChannelListState {
@@ -9,20 +11,97 @@ interface ChannelListState {
   setChannels: (channels: Channel[]) => Promise<void>;
   addChannel: (channel: Channel) => Promise<void>;
   removeChannel: (channel: Channel) => Promise<void>;
+  refreshChannels: () => void;
+  channelsLoading: boolean;
 }
+
+type SetFn = Parameters<StateCreator<ChannelListState>>[0];
+type GetFn = Parameters<StateCreator<ChannelListState>>[1];
+
+
+const fetchChannels = async (set: SetFn, channels: Channel[]) => {
+  let channelSlugs = [];
+  if(channels.length === 0) {
+    const savedChannels = await loadChannels();
+    if(savedChannels.length === 0) {
+      return;
+    }
+
+    channelSlugs = savedChannels;
+  } else {
+    channelSlugs = channels.map((ch: Channel) => ch.slug);
+  }
+
+  let updatedChannels: Channel[] = [];
+
+  set({ channelsLoading: true });
+  getChannels(channelSlugs)
+  .then(async (channels) => {
+    updatedChannels = await fetchChannelUsernames(channels);
+    sortChannels(updatedChannels);
+  }).catch(() => {
+    showErrorChannelsLoading();
+  }).finally(() => {
+    set({ channelsLoading: false, channels: updatedChannels});
+  });
+}
+
+const fetchChannelUsernames = async (channels: Channel[]) => {
+  let error: boolean = false;
+
+  const updatedChannels = await Promise.all(
+    channels.map(async (ch) => {
+      try {
+        const user = await getUser(ch.id);
+        return { ...ch, name: user.name };
+      } catch (err) {
+        error = true;
+        return ch;
+      }
+    })
+  );
+
+  if(error) {
+    showErrorUserLoading();
+  }
+
+  return updatedChannels;
+}
+
+const sortChannels = (channels: Channel[]) => {
+  return channels.sort((a, b) => {
+    if (a.isLive !== b.isLive) {
+      return a.isLive ? -1 : 1;
+    }
+
+    return b.viewerCount - a.viewerCount;
+  });
+}
+
+const refresh = (set: SetFn, get: GetFn) => {
+  fetchChannels(set, get().channels);
+}
+
+const saveChannelsBySlugs = async (channels: Channel[]) => {
+  const slugs = channels.map((ch) => ch.slug);
+  await saveChannels(slugs);
+}
+
 
 export const useChannelListStore = create<ChannelListState>((set, get) => ({
 	channels: [],
   setChannels: async (channels: Channel[]) => {
-    set({ channels });
-    await saveChannels(get().channels);
+    fetchChannels(set, channels);
+    await saveChannelsBySlugs(get().channels);
   },
   addChannel: async (channel: Channel) => {
     set((state) => ({ channels: [...state.channels, channel] }));
-    await saveChannels(get().channels);
+    await saveChannelsBySlugs(get().channels);
   },
   removeChannel: async (channel: Channel) => {
     set((state) => ({ channels: state.channels.filter(ch => ch.id !== channel.id) })),
-    await saveChannels(get().channels);
-  }
+    await saveChannelsBySlugs(get().channels);
+  },
+  refreshChannels: () => refresh(set, get),
+  channelsLoading: false,
 }));
